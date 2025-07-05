b5241acaef0294dfb79edf716e2e7d7c072528dd
Dinushika <dprasangani@gmail.com>
2025-07-05 16:54:12 +0530

    Implement Button Click functionality using Event and Delegate

---
+++ Events/BasicEvent/Button.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public delegate void ClickEventHandler(); // Step 1: Define a delegate
+
+namespace Events.BasicEvent
+{
+    public class Button
+    {
+        public event ClickEventHandler OnClick; // Step 2: Define an event
+
+        public void Click()
+        {
+            Console.WriteLine("Button is clicked.");
+            OnClick?.Invoke();  // Step 3: Raise the event
+        }
+
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Events
+﻿using Events.BasicEvent;
+
+namespace Events
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Button btn = new Button();
+
+            // Subscribe to the event
+            btn.OnClick += RespondToClick;
+
+            btn.Click(); // This triggers the event
+
+            Console.ReadKey();
         }
+    
+     static void RespondToClick()
+        {
+            Console.WriteLine("Event received: Button click handled!");
+        }
+
     }
 }
